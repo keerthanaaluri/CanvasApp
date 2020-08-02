4d8aca98afa42a14dc3498bd0d81f3e9042ee9be
keerthanaaluri <keerthanaaluri@vt.edu>
2020-08-02 14:45:03 -0400

    navigation screen

---
+++ canvapp/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'office-ui-fabric-react/dist/css/fabric.css';
+import './App.css';
+
+import Navigation from './Navigation';
+
+function App() {
+  return (
+    <div className="ms-Grid" dir="ltr">
+      <div className="ms-Grid-row">
+        <div className="ms-Grid-col ms-sm1 ms-xl1">
+          <Navigation />
+        </div>
+        <div className="main-element ms-Grid-col ms-sm11 ms-xl11">
+          <div className="ms-Grid-row">
+
+          </div>
+          <div className="ms-Grid-row">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
